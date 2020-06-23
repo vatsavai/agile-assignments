84e080b7baf8e20aab64eb1d7f3bebee374f1915
Sri Vatsavai <harshinivarma6@gmail.com>
2020-06-23 21:02:37 +0530

    first commit

---
+++ Assignment1/src/helloWorld.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ *  Name: Sri Vatsavai
+ *  Date: 06/23/2020
+ *
+ *  This is a helloWorld program. It
+ *  takes user name from the command line and
+ *  prints "Hello, username!".
+ */
+
+public class helloWorld {
+    public static void main(String[] args) {
+        // prints the output
+        System.out.println("Hello, " + args[0] + "!");
+    }
+}
